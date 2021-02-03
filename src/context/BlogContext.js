f38f5f4fb7e8998beccfa0bfdbb55c60d38a18f9
Jas802 <jas.spencer414@gmail.com>
2021-02-02 19:56:13 -0500

    FlatList to render blog posts

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,7 +3,11 @@ import React from 'react';
 const BlogContext = React.createContext();
 
 export const BlogProvider = ({ children }) => {
-  return <BlogContext.Provider>{children}</BlogContext.Provider>; //add value prop
+  const blogPosts = [{ title: 'Blog POst #1' }, { title: 'Blog Post #2' }];
+
+  return (
+    <BlogContext.Provider value={blogPosts}>{children}</BlogContext.Provider>
+  ); //add value prop
 };
 
 export default BlogContext;
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,13 +1,20 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
-  const value = useContext(BlogContext); //gets value prop form BlogContext
+  const blogPosts = useContext(BlogContext); //gets value prop form BlogContext
 
   return (
     <View>
       <Text>Index Screen</Text>
+      <FlatList data={blogPosts}>
+        keyExtractor={(blogPost) => blogPosts.title}
+        renderItem=
+        {({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      </FlatList>
     </View>
   );
 };
