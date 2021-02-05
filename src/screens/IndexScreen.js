0a7a433ae402e571b5c22600af88b35799b60abd
Jas802 <jas.spencer414@gmail.com>
2021-02-05 12:08:51 -0500

    added delete function for blog posts

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,17 @@
 import React, { useContext } from 'react';
-import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  FlatList,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context); //gets value prop form BlogContext
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context); //gets value prop form BlogContext
 
   return (
     <View>
@@ -16,7 +23,9 @@ const IndexScreen = () => {
           return (
             <View style={styles.row}>
               <Text style={styles.title}>{item.title}</Text>
-              <Feather style={styles.icon} name='trash' />
+              <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                <Feather style={styles.icon} name='trash' />
+              </TouchableOpacity>
             </View>
           );
         }}
